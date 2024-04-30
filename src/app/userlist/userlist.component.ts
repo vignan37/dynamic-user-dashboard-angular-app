d9cfa039d9748a01d0d6df49043986011424906d
vignan37 <vignan37@gmail.com>
2024-04-30 23:16:34 +0530

    added warning banner when id not found

--- src/app/userlist/userlist.component.ts
+++ src/app/userlist/userlist.component.ts
@@ -2,25 +2,26 @@ import { Component, Input } from '@angular/core';
 import { SearchService } from '../services/search.service';
 import { UserDetailsService } from '../services/user-details.service';
 import { UserDetails } from '../models/user-details.model';
+import { CommonModule } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-userlist',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './userlist.component.html',
   styleUrl: './userlist.component.scss'
 })
 export class UserlistComponent {
-
-  searchTerm: string = '';
   userDetails !: UserDetails;
+  snackBarRef: any;
 
-  constructor(private searchService: SearchService, private userDetailsService : UserDetailsService) {
+  constructor(private searchService: SearchService, private userDetailsService : UserDetailsService, private snackBar: MatSnackBar) {
   }
 
   ngOnInit() {
     this.searchService.searchTerm$.subscribe(searchTerm => {
-      this.searchTerm = searchTerm;
       this.searchTheUserDetails(searchTerm);
       console.log("insidengoint"+ searchTerm);
     });
@@ -30,8 +31,27 @@ export class UserlistComponent {
     console.log(searchTerm);
     this.userDetailsService.fetchUserDetails(searchTerm).subscribe((response) =>{
       this.userDetails = response.data;
+      if (this.snackBarRef) {
+        //to remove banner if entered a valid id
+        this.snackBarRef.dismiss();
+      }
       console.log(this.userDetails);
+    },
+    (error) => {
+      this.showErrorMessage('User ID Does not Exist');
     })
+  }
+
+  showErrorMessage(message: string) {
+    this.snackBarRef = this.snackBar.open(message, 'Close', {
+      duration: 5000,
+    });
+    this.snackBarRef
+      .onAction()
+      .pipe(take(1))
+      .subscribe(() => {
+        this.snackBarRef.dismiss();
+      });
   }
 
 }
