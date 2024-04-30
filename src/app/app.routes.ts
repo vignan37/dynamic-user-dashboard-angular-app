beab5752e29d80e5683adcf51e75466553c68a0f
vignan37 <vignan37@gmail.com>
2024-05-01 02:20:25 +0530

    Added routes for user id

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UserlistComponent } from './userlist/userlist.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ { path: '', redirectTo: '/users', pathMatch: 'full' },
+{ path: 'users', component: UserlistComponent }, 
+{ path: 'user-details/:id', component: UserDetailsComponent }];
